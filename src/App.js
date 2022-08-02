6f009a381d36983815cb74546a557100017851da
BishalAshiq <bishaltheboss20@gmail.com>
2022-08-03 00:57:50 +0600

    updated header Items

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
      <Navigation></Navigation>
-     {/* <HeadSection></HeadSection>
-     <MiddleSection></MiddleSection>
+      <HeadSection></HeadSection>
+     {/*<MiddleSection></MiddleSection>
      <BottomSection></BottomSection> */}
     </div>
   );
--- src/Components/HeadSection/HeadSection.js
+++ src/Components/HeadSection/HeadSection.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+  import { faBurger, faUserFriends } from '@fortawesome/free-solid-svg-icons';
+import { Container } from 'react-bootstrap';
+import "./HeadSection.css";
+import pic1 from "../../Images/order.png";
+
+
 
 const HeadSection = () => {
+    const element1 = <FontAwesomeIcon icon={faBurger} />
+    const element2 = <FontAwesomeIcon icon={faUserFriends} />
     return (
-        <div>
-            <h1> Header HeadSection</h1>
+       <Container>
+         <div className='headSec'>
+            <div className='headerItems'>
+                <div>
+                    <h2>459</h2>
+                    <p>Total Minus</p>
+                </div>
+                  {element1}  
+            </div>
+            <div className='headerItems'>
+                <div>
+                    <h2>$ 87,561</h2>
+                    <p>Total Revenue</p>
+                </div>
+                  <h1>$</h1>
+            </div>
+            <div className='headerItems'>
+                <div>
+                    <h2>247</h2>
+                    <p>Total Orders</p>
+                </div>
+                    <img className='pic1' src={pic1} alt=""/>
+            </div>
+            <div className='headerItems'>
+                <div>
+                    <h2>872</h2>
+                    <p>Total Customer</p>
+                </div>
+                  {element2}
+            </div>
         </div>
+       </Container>
     );
 };
 

7b481aafb3d421e6dc8e4595ddaa1cf88f2796fc
BishalAshiq <bishaltheboss20@gmail.com>
2022-08-03 13:03:16 +0600

    updated header Items

--- src/Components/HeadSection/HeadSection.js
+++ src/Components/HeadSection/HeadSection.js
@@ -15,29 +15,29 @@ const HeadSection = () => {
          <div className='headSec'>
             <div className='headerItems'>
                 <div>
-                    <h2>459</h2>
-                    <p>Total Minus</p>
+                    <h2 style={{fontWeight: "bold"}}>459</h2>
+                    <p style={{fontWeight: 700}}>Total Minus</p>
                 </div>
                   <img style={{width: 110}} src={pic1} alt=""/>
             </div>
             <div className='headerItems'>
                 <div>
-                    <h2>$ 87,561</h2>
-                    <p>Total Revenue</p>
+                    <h2 style={{fontWeight: "bold"}}>$ 87,561</h2>
+                    <p style={{fontWeight: 700}}>Total Revenue</p>
                 </div>
                   <img style={{width: 100}} src={pic2} alt=""/>
             </div>
             <div className='headerItems'>
                 <div>
-                    <h2>247</h2>
-                    <p>Total Orders</p>
+                    <h2 style={{fontWeight: "bold"}}>247</h2>
+                    <p style={{fontWeight: 700}}>Total Orders</p>
                 </div>
                     <img className='pic' src={pic3} alt=""/>
             </div>
             <div className='headerItems'>
                 <div>
-                    <h2>872</h2>
-                    <p>Total Customer</p>
+                    <h2 style={{fontWeight: "bold"}}>872</h2>
+                    <p style={{fontWeight: 700}}>Total Customer</p>
                 </div>
                     <img className='pic' src={pic4} alt=""/>
             </div>

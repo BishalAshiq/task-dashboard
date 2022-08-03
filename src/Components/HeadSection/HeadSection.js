553c22f8cb904687f67dabd65ddd9c81a28105a7
BishalAshiq <bishaltheboss20@gmail.com>
2022-08-03 12:58:56 +0600

    updated header Items

--- src/Components/HeadSection/HeadSection.js
+++ src/Components/HeadSection/HeadSection.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-  import { faBurger, faUserFriends } from '@fortawesome/free-solid-svg-icons';
 import { Container } from 'react-bootstrap';
 import "./HeadSection.css";
-import pic1 from "../../Images/order.png";
+import pic1 from "../../Images/food.png";
+import pic2 from "../../Images/doller.png";
+import pic3 from "../../Images/order.png";
+import pic4 from "../../Images/customers.png";
 
 
 
 const HeadSection = () => {
-    const element1 = <FontAwesomeIcon icon={faBurger} />
-    const element2 = <FontAwesomeIcon icon={faUserFriends} />
+   
     return (
        <Container>
          <div className='headSec'>
@@ -18,28 +18,28 @@ const HeadSection = () => {
                     <h2>459</h2>
                     <p>Total Minus</p>
                 </div>
-                  {element1}  
+                  <img style={{width: 110}} src={pic1} alt=""/>
             </div>
             <div className='headerItems'>
                 <div>
                     <h2>$ 87,561</h2>
                     <p>Total Revenue</p>
                 </div>
-                  <h1>$</h1>
+                  <img style={{width: 100}} src={pic2} alt=""/>
             </div>
             <div className='headerItems'>
                 <div>
                     <h2>247</h2>
                     <p>Total Orders</p>
                 </div>
-                    <img className='pic1' src={pic1} alt=""/>
+                    <img className='pic' src={pic3} alt=""/>
             </div>
             <div className='headerItems'>
                 <div>
                     <h2>872</h2>
                     <p>Total Customer</p>
                 </div>
-                  {element2}
+                    <img className='pic' src={pic4} alt=""/>
             </div>
         </div>
        </Container>
